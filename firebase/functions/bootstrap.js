9ccbeadb622675705a4dcfb70285f8f2b3c46204
366 Industries <103216816+Hunting-Fishing@users.noreply.github.com>
2026-08-09 22:18:22 +0800

    Wire gated monetization functions without enabling payments

---
+++ firebase/functions/bootstrap.js
@@ -0,0 +1,62 @@
+"use strict";
+
+// Keep the established function surface intact while layering the marketplace
+// monetization foundation behind separate callables and safety gates.
+const coreExports = require("./index");
+Object.assign(exports, coreExports);
+
+const {onDocumentCreated} = require("firebase-functions/v2/firestore");
+const {onCall} = require("firebase-functions/v2/https");
+const {createAdminRuntime} = require("./admin_runtime");
+const {protectedCallableOptions} = require("./app_check_config");
+const {createAffiliateCommands} = require("./affiliate_commands");
+const {
+  createStripeMarketplaceCommands,
+  stripeSecretKey,
+} = require("./stripe_marketplace_commands");
+const {
+  createMarketplaceMonetization,
+} = require("./marketplace_monetization");
+
+const admin = createAdminRuntime();
+const affiliateCommands = createAffiliateCommands(admin);
+const stripeMarketplaceCommands = createStripeMarketplaceCommands(admin);
+const marketplaceMonetization = createMarketplaceMonetization(admin);
+
+exports.ensureAffiliateCode = onCall(
+    protectedCallableOptions,
+    affiliateCommands.ensureAffiliateCode,
+);
+exports.claimAffiliateReferral = onCall(
+    protectedCallableOptions,
+    affiliateCommands.claimAffiliateReferral,
+);
+
+const stripeCallableOptions = Object.freeze({
+  ...protectedCallableOptions,
+  secrets: [stripeSecretKey],
+});
+
+exports.ensureStripeSellerAccount = onCall(
+    stripeCallableOptions,
+    stripeMarketplaceCommands.ensureStripeSellerAccount,
+);
+exports.createStripeSellerOnboardingLink = onCall(
+    stripeCallableOptions,
+    stripeMarketplaceCommands.createStripeSellerOnboardingLink,
+);
+exports.refreshStripeSellerStatus = onCall(
+    stripeCallableOptions,
+    stripeMarketplaceCommands.refreshStripeSellerStatus,
+);
+
+exports.onMarketplaceTransactionCreatedMonetization = onDocumentCreated(
+    {
+      document: "marketplace_transactions/{transactionId}",
+      retry: true,
+    },
+    async (event) => marketplaceMonetization.snapshotAcceptedTransaction({
+      transactionId: event.params.transactionId,
+      transactionData: event.data && event.data.data(),
+    }),
+);
