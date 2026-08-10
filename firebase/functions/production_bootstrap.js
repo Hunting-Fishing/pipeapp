afd89d0c1a2fd9fc4408e334e50cde5eb7940e0c
366 Industries <103216816+Hunting-Fishing@users.noreply.github.com>
2026-08-10 11:55:17 +0800

    Expose MFA-admin payment readiness callables

---
+++ firebase/functions/production_bootstrap.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// Preserve the complete marketplace function surface and add the MFA-admin
+// production-readiness control plane as a thin wrapper.
+const coreExports = require("./bootstrap");
+Object.assign(exports, coreExports);
+
+const {onCall} = require("firebase-functions/v2/https");
+const {createAdminRuntime} = require("./admin_runtime");
+const {protectedCallableOptions} = require("./app_check_config");
+const {
+  createPaymentReadinessAdmin,
+} = require("./payment_readiness_admin");
+
+const readinessAdmin = createPaymentReadinessAdmin(createAdminRuntime());
+
+exports.getPaymentProviderReadiness = onCall(
+    protectedCallableOptions,
+    readinessAdmin.getPaymentProviderReadiness,
+);
+
+exports.setPaymentProviderReadiness = onCall(
+    protectedCallableOptions,
+    readinessAdmin.setPaymentProviderReadiness,
+);
