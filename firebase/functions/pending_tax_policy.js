3b5b7958d1b73a05d0b9721c5fa5fa3b9b99e4c8
366 Industries <103216816+Hunting-Fishing@users.noreply.github.com>
2026-08-10 14:26:32 +0800

    Add provisional tax reserve policy for pending registration

---
+++ firebase/functions/pending_tax_policy.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const BASIS_POINTS = 10000;
+const PROVISIONAL_TAX_RESERVE_BPS = 1500;
+
+function taxCollectionStatus(readiness = {}) {
+  if (readiness.stripeTaxReady === true) return "registered";
+  if (readiness.stripeTaxRegistrationPending === true) {
+    return "registration_pending";
+  }
+  return "not_ready";
+}
+
+function automaticTaxEnabled(readiness = {}) {
+  return readiness.stripeTaxReady === true;
+}
+
+function provisionalTaxReserveMinor(baseMinor, status) {
+  const amount = Number(baseMinor);
+  if (status !== "registration_pending" ||
+      !Number.isSafeInteger(amount) || amount <= 0) {
+    return 0;
+  }
+  return Math.ceil(amount * PROVISIONAL_TAX_RESERVE_BPS / BASIS_POINTS);
+}
+
+module.exports = {
+  BASIS_POINTS,
+  PROVISIONAL_TAX_RESERVE_BPS,
+  automaticTaxEnabled,
+  provisionalTaxReserveMinor,
+  taxCollectionStatus,
+};
