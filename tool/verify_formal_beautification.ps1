d2755020b3d2bf6b18fb67e1b45a9f3ac46a6dfc
366 Industries <103216816+Hunting-Fishing@users.noreply.github.com>
2026-08-15 23:20:21 +0800

    Verify Timed Buying presentation contracts

--- tool/verify_formal_beautification.ps1
+++ tool/verify_formal_beautification.ps1
@@ -34,6 +34,7 @@ try {
     'test/pipe_buyer_commerce_components_test.dart',
     'test/pipe_buyer_browse_components_test.dart',
     'test/pipe_buyer_analytics_components_test.dart',
+    'test/marketplace_timed_buying_presentation_test.dart',
     'test/pipe_buyer_formal_sequence_test.dart',
     'test/pipe_buyer_theme_test.dart'
   ) | Where-Object { Test-Path $_ }
