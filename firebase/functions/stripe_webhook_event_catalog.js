7811023893545468f9fd7dca6749f2fdb2e6e5d1
366 Industries <103216816+Hunting-Fishing@users.noreply.github.com>
2026-08-21 23:44:30 +0800

    Add authoritative Stripe webhook event catalog

---
+++ firebase/functions/stripe_webhook_event_catalog.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const STRIPE_WEBHOOK_EVENTS = Object.freeze([
+  "checkout.session.completed",
+  "checkout.session.async_payment_succeeded",
+  "checkout.session.async_payment_failed",
+  "invoice.paid",
+  "invoice.payment_failed",
+  "customer.subscription.created",
+  "customer.subscription.updated",
+  "customer.subscription.deleted",
+  "charge.refunded",
+  "refund.created",
+  "refund.updated",
+  "refund.failed",
+  "charge.dispute.created",
+  "charge.dispute.updated",
+  "charge.dispute.closed",
+  "charge.dispute.funds_withdrawn",
+  "charge.dispute.funds_reinstated",
+]);
+
+module.exports = {STRIPE_WEBHOOK_EVENTS};
