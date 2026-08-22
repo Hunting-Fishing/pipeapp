fb6673bb9d2c71311e6ba05e363ad7e6847ac390
366 Industries <103216816+Hunting-Fishing@users.noreply.github.com>
2026-08-23 00:04:18 +0800

    Support Canadian small supplier tax mode

--- firebase/functions/pending_tax_policy.js
+++ firebase/functions/pending_tax_policy.js
@@ -5,6 +5,9 @@ const PROVISIONAL_TAX_RESERVE_BPS = 1500;
 
 function taxCollectionStatus(readiness = {}) {
   if (readiness.stripeTaxReady === true) return "registered";
+  if (readiness.canadaGstHstSmallSupplier === true) {
+    return "small_supplier_unregistered";
+  }
   if (readiness.stripeTaxRegistrationPending === true) {
     return "registration_pending";
   }
@@ -13,6 +16,7 @@ function taxCollectionStatus(readiness = {}) {
 
 function taxBillingPrepared(readiness = {}) {
   return readiness.stripeTaxReady === true ||
+    readiness.canadaGstHstSmallSupplier === true ||
     (readiness.stripeTaxRegistrationPending === true &&
       readiness.stripeTaxPendingBillingApproved === true);
 }
