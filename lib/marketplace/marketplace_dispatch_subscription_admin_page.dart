8b4ebc12bcfa375e24e7d9576e3a78070567f4ae
366 Industries <103216816+Hunting-Fishing@users.noreply.github.com>
2026-08-23 13:11:03 +0800

    Add provider Portal control to Dispatch operations

--- lib/marketplace/marketplace_dispatch_subscription_admin_page.dart
+++ lib/marketplace/marketplace_dispatch_subscription_admin_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'marketplace_admin_access.dart';
+import 'marketplace_dispatch_billing_portal_control.dart';
 import 'marketplace_dispatch_subscription_admin_panel.dart';
 import 'marketplace_dispatch_subscription_launch_readiness_panel.dart';
 
@@ -80,6 +81,8 @@ class _MarketplaceDispatchSubscriptionAdminPageState
               padding: const EdgeInsets.all(16),
               children: const [
                 MarketplaceDispatchSubscriptionLaunchReadinessPanel(),
+                SizedBox(height: 12),
+                MarketplaceDispatchBillingPortalControl(),
                 SizedBox(height: 12),
                 _DispatchReconciliationExplanation(),
                 SizedBox(height: 12),
