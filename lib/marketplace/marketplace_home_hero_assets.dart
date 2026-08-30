9972e0550a178ef978e3e0b10c35ea4a8673eb03
366 Industries <103216816+Hunting-Fishing@users.noreply.github.com>
2026-08-30 17:03:50 +0800

    Use JPEG hero assets for reliable Flutter decoding

--- lib/marketplace/marketplace_home_hero_assets.dart
+++ lib/marketplace/marketplace_home_hero_assets.dart
@@ -2,9 +2,9 @@ class MarketplaceHomeHeroAssets {
   const MarketplaceHomeHeroAssets._();
 
   static const String desktopBackground =
-      'assets/images/marketplace_home_hero_desktop.webp';
+      'assets/images/marketplace_home_hero_desktop.jpg';
   static const String mobileBackground =
-      'assets/images/marketplace_home_hero_mobile.webp';
+      'assets/images/marketplace_home_hero_mobile.jpg';
 
   static const double mobileBreakpoint = 720;
   static const double desktopMinHeight = 440;
