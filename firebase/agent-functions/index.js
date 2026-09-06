88109d91d5eb912befe47887de4595db435acc84
366 Industries <103216816+Hunting-Fishing@users.noreply.github.com>
2026-09-06 20:37:09 +0800

    Yellow Pages: expose protected public and contact-center callables

--- firebase/agent-functions/index.js
+++ firebase/agent-functions/index.js
@@ -2,23 +2,31 @@
 
 const functions = require("firebase-functions/v1");
 const {getApps, initializeApp} = require("firebase-admin/app");
+const {getAuth} = require("firebase-admin/auth");
 const {FieldValue, getFirestore} = require("firebase-admin/firestore");
 const {
   AgentPolicyError,
   createAgentHandler,
   loadAgentConfiguration,
 } = require("./agent_policy");
+const {
+  YellowPagesError,
+  createYellowPagesCommands,
+} = require("./yellow_pages_commands");
 
 if (getApps().length === 0) initializeApp();
 
+const db = getFirestore();
+const auth = getAuth();
 const configuration = loadAgentConfiguration();
 const handler = createAgentHandler({
   configuration,
-  writeAudit: async (event) => getFirestore()
+  writeAudit: async (event) => db
       .collection("administrator_agent_audit")
       .doc(event.requestId)
       .create({...event, createdAt: FieldValue.serverTimestamp()}),
 });
+const yellowPages = createYellowPagesCommands({db, auth});
 
 exports.agent = functions
     .runWith({
@@ -44,3 +52,88 @@ exports.agent = functions
         );
       }
     });
+
+function yellowPagesCallable(name, command) {
+  return functions
+      .runWith({
+        enforceAppCheck: true,
+        memory: "256MB",
+        minInstances: 0,
+        maxInstances: 20,
+        timeoutSeconds: 30,
+      })
+      .https.onCall(async (data, context) => {
+        try {
+          return await command(data || {}, context);
+        } catch (error) {
+          if (error instanceof YellowPagesError) {
+            throw new functions.https.HttpsError(error.code, error.message);
+          }
+          functions.logger.error("PipeBuyer Yellow Pages callable failed.", {
+            functionName: name,
+            errorName: error && error.name,
+          });
+          throw new functions.https.HttpsError(
+              "internal",
+              "The PipeBuyer Yellow Pages service could not complete this request.",
+          );
+        }
+      });
+}
+
+exports.getYellowPagesAccess = yellowPagesCallable(
+    "getYellowPagesAccess",
+    yellowPages.getAccess,
+);
+exports.listPipeBuyerYellowPages = yellowPagesCallable(
+    "listPipeBuyerYellowPages",
+    yellowPages.listPublic,
+);
+exports.getPipeBuyerYellowPagesEntry = yellowPagesCallable(
+    "getPipeBuyerYellowPagesEntry",
+    yellowPages.getPublic,
+);
+exports.listYellowPagesCompanies = yellowPagesCallable(
+    "listYellowPagesCompanies",
+    yellowPages.listCompanies,
+);
+exports.getYellowPagesCompany = yellowPagesCallable(
+    "getYellowPagesCompany",
+    yellowPages.getCompany,
+);
+exports.upsertYellowPagesCompany = yellowPagesCallable(
+    "upsertYellowPagesCompany",
+    yellowPages.upsertCompany,
+);
+exports.assignYellowPagesCompany = yellowPagesCallable(
+    "assignYellowPagesCompany",
+    yellowPages.assignCompany,
+);
+exports.recordYellowPagesContactEvent = yellowPagesCallable(
+    "recordYellowPagesContactEvent",
+    yellowPages.recordContactEvent,
+);
+exports.setYellowPagesDoNotContact = yellowPagesCallable(
+    "setYellowPagesDoNotContact",
+    yellowPages.setDoNotContact,
+);
+exports.reviewYellowPagesVerification = yellowPagesCallable(
+    "reviewYellowPagesVerification",
+    yellowPages.reviewVerification,
+);
+exports.setYellowPagesBillingStatus = yellowPagesCallable(
+    "setYellowPagesBillingStatus",
+    yellowPages.setBillingStatus,
+);
+exports.setYellowPagesPublicationStatus = yellowPagesCallable(
+    "setYellowPagesPublicationStatus",
+    yellowPages.setPublicationStatus,
+);
+exports.listYellowPagesStaff = yellowPagesCallable(
+    "listYellowPagesStaff",
+    yellowPages.listStaff,
+);
+exports.manageYellowPagesStaff = yellowPagesCallable(
+    "manageYellowPagesStaff",
+    yellowPages.manageStaff,
+);
